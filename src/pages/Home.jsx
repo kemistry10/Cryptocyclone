d3d8adcccb29837af70026bc64d05312309d03ea
KETAN MISTRY <km@ketans-air.home>
2024-09-27 19:29:59 +0100

    minor change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import './Home.css'
-import { CoinContext } from '../../context/CoinContext'
+import { CoinContext } from '../context/CoinContext'
 import { Link } from 'react-router-dom' /* open another page */
 
 const Home = () => {
