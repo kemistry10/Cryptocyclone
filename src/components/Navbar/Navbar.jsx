d19ed964bb8d12b5040eac45cd878f24bcb329a0
KETAN MISTRY <km@ketans-air.home>
2024-10-01 15:57:25 +0100

    logo taken out

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react'
 import './Navbar.css'
-import logo from '/Users/km/Desktop/Projects/Cryptocyclone/src/assets/Crpytocyclone_logo.png'
-import arrow_icon from '/Users/km/Desktop/Projects/Cryptocyclone/src/assets/arrow_icon.png'
+import arrow_icon from '/src/assets/arrow_icon.png'
 import { CoinContext } from '../../context/CoinContext'
 import { Link } from 'react-router-dom'
 
